47705fb2e9214379afa9a88114c728ba469b944b
DNibs <dnibs513@gmail.com>
2021-06-08 18:27:18 -0400

    completely revamped approach to match gpu implementation

--- main.py
+++ main.py
@@ -2,57 +2,55 @@
 # Author: David Niblick
 # 06JUN2021
 
-import cmath
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-PIXEL_LENGTH = 500
-CENTER_PT = (-.5, -.33)
-M_SCALE = 0.2
-
-
-class Mandelbrot():
-    def __init__(self, pixel_length, center=(-1., 1.), scale=1., bailout=4., max_iterations=1000):
-        self.pixel_length = pixel_length
-        self.img = np.zeros([self.pixel_length, self.pixel_length])
-        self.center = center
-        self.scale = scale
-        self.bailout = bailout
-        self.max_iterations = max_iterations
-        self.top_left = (center[0]-(scale/2), center[1]-(scale/2))
-
-    def map_pixel(self, pixel):
-        x = self.top_left[0] + (pixel[0] * self.scale)/self.pixel_length
-        y = self.top_left[1] - (pixel[1] * self.scale)/self.pixel_length
-        return complex(x, y)
-
-    def sample_image(self):
-        for m in range(0, self.pixel_length):
-            print('\rImage {0:2.0f}% complete'.format(100*m/self.pixel_length), end='')
-            for n in range(0, self.pixel_length):
-                count = 0
-                c = self.map_pixel((m, n))
-                z = 0
-                while abs(z) < self.bailout and count < self.max_iterations:
-
-                    z = z**2 + c
-                    count += 1
-                self.img[n, m] = count / self.max_iterations
-
-    def diplay_img(self):
-        fig1 = plt.figure()
-        plt.imshow(self.img)
-        plt.show()
-
-    def sample_and_display(self):
-        self.sample_image()
-        self.diplay_img()
+# todo: fix axis - mismatch between x/y axis, but as long as img is square it's not issue
+XMIN = -1.
+XMAX = 1.
+YMIN = -1.
+YMAX = 1.
+ROW = 1000
+COL = 1000
+NUM_TICKS = 10
+ITERATIONS = 500
+BAILOUT = 4
 
 
-# Press the green button in the gutter to run the script.
+def linear_mapping(location, old_low, old_high, new_low, new_high):
+    return (location - old_low) / (old_high - old_low) * (new_high - new_low) + new_low
+
+
+def sample_mandelbrot(img_array, x_low, x_high, y_low, y_high):
+    x_axis_pixels, y_axis_pixels = np.shape(img_array)
+    for m in range(0, x_axis_pixels):
+        print('\rImage {0:2.0f}% complete'.format(100 * m / x_axis_pixels), end='')
+        for n in range(0, y_axis_pixels):
+            count = 0
+            z = 0
+            x_val = linear_mapping(m, 0, x_axis_pixels, x_low, x_high)
+            y_val = linear_mapping(n, 0, y_axis_pixels, y_low, y_high)
+            c = x_val + y_val*1.j
+            while count < ITERATIONS and abs(z) < BAILOUT:
+                count += 1
+                z = z**2 + c
+            img_array[m, n] = count
+
+
+def display_image(img_array):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xticks(np.arange(0, ROW, ROW / NUM_TICKS))
+    ax.set_yticks(np.arange(0, COL, COL / NUM_TICKS))
+    xlabels = np.arange(XMIN, XMAX, (XMAX - XMIN) / NUM_TICKS)[0:NUM_TICKS]
+    ax.set_xticklabels([str(round(float(xlabel), 5)) for xlabel in xlabels], rotation=45, ha='right')
+    ylabels = np.arange(YMIN, YMAX, (YMAX - YMIN) / NUM_TICKS)[0:NUM_TICKS]
+    ax.set_yticklabels([str(round(float(ylabel), 5)) for ylabel in ylabels])
+    im = ax.imshow(img_array.T, cmap="RdBu", interpolation="bilinear")
+    plt.show()
+
+
 if __name__ == '__main__':
-    mandelbrot1 = Mandelbrot(pixel_length=PIXEL_LENGTH, center=CENTER_PT, scale=M_SCALE)
-    mandelbrot1.sample_and_display()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mandelbrot_img = np.zeros([COL, ROW])
+    sample_mandelbrot(mandelbrot_img, XMIN, XMAX, YMIN, YMAX)
+    display_image(mandelbrot_img)
